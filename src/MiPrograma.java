52436e80e95b152ff9f215758619db269548a51f
Dicxie Madrigal <hidannara@yahoo.es>
2021-06-12 17:33:43 -0600

    Inicio de repositorio

---
+++ src/MiPrograma.java
@@ -0,0 +1,11 @@
+/*
+ * Programa de ejemplo que describe las estructura básicas de 
+ * Java
+ */
+public class MiPrograma {
+
+	public static void main(String[] args) { // funcion principal que comienza todo programa de java
+		System.out.println("Hola Mundo!"); // Muestra un mensaje por pantalla 
+	}
+
+}
